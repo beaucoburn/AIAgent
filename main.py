86c2897176bddadddac0f2acd80cabdb948c2ea4
beaucoburn <douglas.coburn@gmail.com>
2025-07-03 10:47:38 -0600

    modified:   main.py

--- main.py
+++ main.py
@@ -6,12 +6,11 @@ client = genai.Client(api_key=api_key)
 
 response = client.models.generate_content( model="gemini-2.0-flash-001", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-
 def main():
     print("Hello from aiagent!")
 
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
 
 if __name__ == "__main__":
     main()
