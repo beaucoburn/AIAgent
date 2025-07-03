1354ca51f1b0676b3b15982beb1e98dce948324f
beaucoburn <douglas.coburn@gmail.com>
2025-07-03 11:40:05 -0600

    modified:   main.py

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 
 def main():
     print("Hello from aiagent!")
 
+    if len(sys.argv) < 2:
+        print("Error: Please provide a prompt as a command line argument.")
+        print("Usage: python3 main.py \"Your prompt here\"")
+        sys.exit(1)
+
+    prompt = sys.argv[1]
+
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
 
     client = genai.Client(api_key=api_key)
 
-    response = client.models.generate_content( model="gemini-2.0-flash-001", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+    response = client.models.generate_content( model="gemini-2.0-flash-001", contents=prompt)
 
     print(response.text)
 
