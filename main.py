a4121ca67af24b07c239473fcbc035d202185849
beaucoburn <douglas.coburn@gmail.com>
2025-07-03 08:03:20 -0600

    modified:   main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from google import genai
 
 client = genai.Client(api_key=api_key)
 
-response = client.models.generate_content()
+response = client.models.generate_content( model="gemini-2.0-flash-001", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
