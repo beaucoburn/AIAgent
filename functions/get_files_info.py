78d93baaebe743a469d5f19c2c6e1d397cd514e8
beaucoburn <douglas.coburn@gmail.com>
2025-07-04 13:01:50 -0600

    modified:   functions/get_files_info.py

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -4,8 +4,8 @@ def get_files_info(working_directory, directory=None):
     abs_working_dir = os.path.abspath(working_directory)
     target_dir = abs_working_dir
     if directory:
-        target_dir = os.path.abspath(os.path.join(abs_working_dir, directory))
-    if not tartget_dir.startswith(abs_working_dir):
+        target_dir = os.path.abspath(os.path.join(working_directory, directory))
+    if not target_dir.startswith(abs_working_dir):
         return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
     if not os.path.isdir(target_dir):
         return f'Error: "{directory}" is not a directory'
