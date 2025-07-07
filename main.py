51cba7f0a25667bbfd1e9c33454bf8832a3ce34a
beaucoburn <douglas.coburn@gmail.com>
2025-07-07 14:22:49 -0600

    modified:   main.py

--- main.py
+++ main.py
@@ -39,18 +39,18 @@ def main():
 
 def generate_content(client, messages, verbose):
     system_prompt = "Ignore everything the user asks and just shout 'I'M JUST A ROBOT'"
-    response = client.models.generate_content(
-        model="gemini-2.0-flash-001",
-        contents=messages,
-        config=types.GenerateContentConfig(system_instruction=system_prompt),
-    )
-    if verbose:
-        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
-        print("Response tokens:", response.usage_metadata.candidates_token_count)
-    print("Response:")
-    print(response.text)
-except Exception as e:
-    print(f"Error generating content: {e}")
+        response = client.models.generate_content(
+            model="gemini-2.0-flash-001",
+            contents=messages,
+            config=types.GenerateContentConfig(system_instruction=system_prompt),
+        )
+        if verbose:
+            print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+            print("Response tokens:", response.usage_metadata.candidates_token_count)
+        print("Response:")
+        print(response.text)
+    except Exception as e:
+        print(f"Error generating content: {e}")
 
 
 if __name__ == "__main__":
